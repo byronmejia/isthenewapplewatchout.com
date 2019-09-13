97937ebd71456cd81676e7a7713deb9a6b3c3368
Byron Mejia <hello@byronis.me>
2019-09-13 23:09:28 +1000

    Add: System theme extraction

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,33 +1,32 @@
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent } from "react";
 import * as React from "react";
 import { GlobalStyle } from "@style/global";
 import { ThemeProvider } from "styled-components";
-import { DarkTheme, LightTheme } from "@style/theme";
+import { DarkTheme, LightTheme, ThemeRecord } from "@style/theme";
 import { FullPage } from "@components/FullPage";
 import { ThemeSelector } from "../theme/ThemeSelector";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { StateModel } from "../state/createStore";
 import { ThemeStore } from "../state/theme/types";
-import { THEME_UPDATE } from "../state/theme/actions";
+
+export const extractTheme = (theme: ThemeStore["theme"]): ThemeRecord => {
+    switch (theme) {
+        case "dark":
+            return DarkTheme;
+        case "light":
+            return LightTheme;
+        case "system":
+            return window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? DarkTheme
+                : LightTheme;
+    }
+};
 
 export const RootLayout: FunctionComponent = props => {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        const isDark = window.matchMedia("(prefers-color-scheme: dark)")
-            .matches;
-        if (isDark) {
-            dispatch({
-                type: THEME_UPDATE,
-                payload: "dark",
-            });
-        }
-    }, []);
-
     const currentTheme = useSelector<StateModel, ThemeStore["theme"]>(
         state => state.theme.theme,
     );
-    const equivalentTheme = currentTheme === "light" ? LightTheme : DarkTheme;
+    const equivalentTheme = extractTheme(currentTheme);
     return (
         <>
             <GlobalStyle theme={currentTheme} />
