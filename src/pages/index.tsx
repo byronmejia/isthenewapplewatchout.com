aa87e8427c03f88802c02f3eeea1f4b2c36b572e
Byron Mejia <hello@byronis.me>
2019-09-18 21:43:04 +1000

    feat(main): add <title> tag (#5) (#12)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { RootLayout } from "@components/RootLayout";
 import { Header } from "@components/Header";
 import { Paragraph } from "@components/Paragraph";
+import SEO from "@components/seo";
 
 const IndexPage = () => (
     <RootLayout>
+        <SEO
+            title="Is The New Apple Watch Out Yet"
+            keywords={["apple", "health", "watch", "fitness", "life", "blog"]}
+            description="Please, is it out yet? Apple products cures my crippling depression..."
+        />
         <Header>Apple Watch Series 5 Status</Header>
         <Paragraph>Pre Order</Paragraph>
     </RootLayout>
