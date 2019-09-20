dc09b27dae0654f6e1bf35905ed896595e3d2769
Byron Mejia <hello@byronis.me>
2019-09-20 21:11:49 +1000

    feat(main): add system theme via media queries and css variables)

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,15 +1,52 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { ThemeStore } from "../state/theme/types";
-import { DarkTheme, LightTheme } from "@style/theme";
+import { DarkTheme, LightTheme, SystemTheme } from "@style/theme";
 
 export interface GlobalStyleProps {
     theme: ThemeStore["theme"];
 }
 
+export function inferBackgroundColor(theme: ThemeStore["theme"]): string {
+    switch (theme) {
+        case "dark":
+            return DarkTheme.primaryBackground;
+        case "light":
+            return LightTheme.primaryBackground;
+        case "system":
+            return SystemTheme.primaryBackground;
+    }
+}
+
+export function inferColor(theme: ThemeStore["theme"]): string {
+    switch (theme) {
+        case "dark":
+            return DarkTheme.primaryColor;
+        case "light":
+            return LightTheme.primaryColor;
+        case "system":
+            return SystemTheme.primaryColor;
+    }
+}
+
 export const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
+  :root {
+     --primary-background: ${LightTheme.primaryBackground};
+     --secondary-background: ${LightTheme.secondaryBackground};
+     --primary-color: ${LightTheme.primaryColor};
+     --secondary-color: ${LightTheme.secondaryColor};
+     
+     @media (prefers-color-scheme: dark) {
+       --primary-background: ${DarkTheme.primaryBackground};
+       --secondary-background: ${DarkTheme.secondaryBackground};
+       --primary-color: ${DarkTheme.primaryColor};
+       --secondary-color: ${DarkTheme.secondaryColor};
+     }
+  }
+   
    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
    ${reset}
+   
    * {
       box-sizing: border-box;
    }
@@ -19,13 +56,7 @@ export const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
    }
    body {
       transition: background-color, color 300ms ease-out;
-      background-color: ${props =>
-          props.theme === "light"
-              ? LightTheme.primaryBackground
-              : DarkTheme.primaryBackground};
-      color: ${props =>
-          props.theme === "light"
-              ? LightTheme.primaryColor
-              : DarkTheme.secondaryColor};
+      background-color: ${props => inferBackgroundColor(props.theme)};
+      color: ${props => inferColor(props.theme)};
    }
 `;
